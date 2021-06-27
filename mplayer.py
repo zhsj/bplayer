bcaad5b523cbc3723aaa40b7d9e72c8aa4fdfe3c
Shengjing Zhu <i@zhsj.me>
2021-06-27 21:45:09 +0800

    change vpx id

--- mplayer.py
+++ mplayer.py
@@ -5,7 +5,7 @@ import re
 import logging
 import os.path
 
-VPX = "16"
+VPX = ""
 UA = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 
 
